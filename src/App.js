36c252da78100fd6496ff42bde75fa518d17283e
mstars_lab2_07 <mstars_lab2_07@mstars-lab2-07s-iMac.local>
2022-03-28 17:26:30 +0800

    removed logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import NavbarComponent from './components/NavbarComponent';
 import CarouselComp from './components/CarouselComp';
